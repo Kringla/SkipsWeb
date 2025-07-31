dd171463489dd4b53f45a60efd1c1b81036507fd
Kringla <41374076+Kringla@users.noreply.github.com>
2025-07-31 18:56:41 +0200

    Endret config.php til bootstrap.php

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once "includes/config.php";
+require_once __DIR__ . '/includes/bootstrap.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"] ?? '';
--- register.php
+++ register.php
@@ -1,5 +1,5 @@
 <?php
-require_once "includes/config.php";
+require_once __DIR__ . '/includes/bootstrap.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"] ?? '';
